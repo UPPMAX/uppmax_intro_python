1c3c0d75d5f1656b6bddad27b7f06938d5714960
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-01-17 10:30:27 +0100

    Describe data

--- docs/data/20250307/README.md
+++ docs/data/20250307/README.md
@@ -4,6 +4,9 @@
 
 - Data: [n_registrations_in_time.csv](n_registrations_in_time.csv)
 - Code: [plot_n_registrations_in_time.R](plot_n_registrations_in_time.R)
+    - `date`: the date, in `YYYYMMDD` format
+    - `n_registrations`: the total amount of registrations
+      at the end of that day
 
 ![Number of registrations in time](n_registrations_in_time.png)
 
--- docs/data/20250307/plot_n_registrations_in_time.R
+++ docs/data/20250307/plot_n_registrations_in_time.R
@@ -1,14 +1,29 @@
 #!/bin/env Rscript
 
+course_date <- tail(stringr::str_split(getwd(), pattern = "/")[[1]], n = 1)
+
 registrations_in_time <- readr::read_csv(
   "n_registrations_in_time.csv",
   show_col_types = FALSE
 )
 
+testthat::expect_true("n_registrations" %in% names(registrations_in_time))
+testthat::expect_true("date" %in% names(registrations_in_time))
+
+max_n_registrations <- max(registrations_in_time$n_registrations)
+
 ggplot2::ggplot(
   registrations_in_time,
   ggplot2::aes(x = date, y = n_registrations)
 ) + ggplot2::geom_line() + 
-  ggplot2::scale_y_continuous(limits = c(0, NA))
+  ggplot2::scale_y_continuous(
+    limits = c(0, max_n_registrations),
+    breaks = seq(0, max_n_registrations)
+  ) +
+  ggplot2::labs(
+    title = "Number of course registrations in time",
+    subtitle = paste0("Course date: ", course_date)
+    
+  )
 
 ggplot2::ggsave("n_registrations_in_time.png", width = 7, height = 7)
