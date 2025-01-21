e95693a5284ca5681d9a45b163750c113a5be036
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-01-21 09:32:04 +0100

    Add registrations

--- docs/data/20250307/convert_google_doc_to_csv.R
+++ docs/data/20250307/convert_google_doc_to_csv.R
@@ -4,8 +4,22 @@
 # registrations and email addresses to a
 # `date,n_registrations` format
 
-t <- readr::read_csv("~/Downloads/Intro to Python 2025-03-07 (Responses) - Form responses 1.csv")
-names(t) <- c("timestamp", "email")
+t_all <- readr::read_csv("~/Downloads/responses.csv")
+names(t_all) <- c("timestamp", "email")
 
-# Do by hand, this is too much work :-)
+t_all$email <- cli::hash_md5(t_all$email)
 
+t <- t_all |> dplyr::distinct(email, .keep_all = TRUE)
+
+readr::write_csv(t, "registrations.csv")
+
+t$date <- stringr::str_split_fixed(t$timestamp, pattern = " ", n = 2)[, 1]
+
+n_registrations <- t |> 
+  dplyr::select_all() |>
+  dplyr::group_by(date) |> 
+  dplyr::count()
+
+n_registrations$cumulative <- cumsum(n_registrations$n)
+
+readr::write_csv(n_registrations, "n_registrations_in_time.csv")
--- docs/data/20250307/plot_n_registrations_in_time.R
+++ docs/data/20250307/plot_n_registrations_in_time.R
@@ -1,28 +1,37 @@
 #!/bin/env Rscript
+
+if (1 == 2) {
+  readr::read_lines("n_registrations_in_time.csv")
+}
 registrations_in_time <- readr::read_csv(
   file = "n_registrations_in_time.csv",
   col_types = readr::cols(
-    date = readr::col_date(format = "%Y%m%d"),
-    n_registrations = readr::col_double()
+    date = readr::col_date(format = "%d/%m/%Y"),
+    n = readr::col_double(),
+    cumulative = readr::col_double()
   )
 )
+if (1 == 2) {
+  vroom::problems(registrations_in_time)
+}
 
-testthat::expect_true("n_registrations" %in% names(registrations_in_time))
+testthat::expect_true("n" %in% names(registrations_in_time))
+testthat::expect_true("cumulative" %in% names(registrations_in_time))
 testthat::expect_true("date" %in% names(registrations_in_time))
 
 course_date <- readr::parse_date(
   tail(
     stringr::str_split(getwd(), pattern = "/")[[1]],
     n = 1
-  ), 
+  ),
   format = "%Y%m%d")
 
 min_date <- min(registrations_in_time$date)
-max_n_registrations <- max(registrations_in_time$n_registrations)
+max_n_registrations <- max(registrations_in_time$cumulative)
 
 ggplot2::ggplot(
   registrations_in_time,
-  ggplot2::aes(x = date, y = n_registrations)
+  ggplot2::aes(x = date, y = cumulative)
 ) + ggplot2::geom_line() + 
   ggplot2::scale_x_date(
     limits = c(min_date, course_date)
@@ -36,6 +45,5 @@ ggplot2::ggplot(
     subtitle = paste0("Course date: ", course_date)
   ) 
   
-
 
 ggplot2::ggsave("n_registrations_in_time.png", width = 7, height = 7)
