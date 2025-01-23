fc6ea596356ee851e7766ce328c28b7680b08f2a
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-01-23 10:44:48 +0100

    Show expected amount

--- docs/data/20250304/plot_n_registrations_in_time.R
+++ docs/data/20250304/plot_n_registrations_in_time.R
@@ -19,6 +19,11 @@ testthat::expect_true("n" %in% names(registrations_in_time))
 testthat::expect_true("cumulative" %in% names(registrations_in_time))
 testthat::expect_true("date" %in% names(registrations_in_time))
 
+# Add today
+registrations_in_time <- registrations_in_time |> tibble::add_row(
+  date = lubridate::today(), n = 0, cumulative = max(registrations_in_time$cumulative)
+)
+
 course_date <- readr::parse_date(
   tail(
     stringr::str_split(getwd(), pattern = "/")[[1]],
@@ -47,3 +52,45 @@ ggplot2::ggplot(
   
 
 ggplot2::ggsave("n_registrations_in_time.png", width = 7, height = 7)
+
+
+## Scribbles
+testthat::expect_equal(1, 2)
+
+
+if (1 == 2) {
+  t <- registrations_in_time
+  fit <- lm(cumulative ~ date, data = t)
+  x_axis <- t$date
+  plot(t$date, t$cumulative)
+  lines(x_axis, predict(fit))
+}
+
+if (1 == 2) {
+  ind <- "date"
+  dep <- "cumulative"
+  t <- registrations_in_time
+  y <- t$cumulative
+  x <- as.double(t$date - min(t$date))
+  x_max <- as.double(course_date - min(t$date))
+  log.ss <- nls(y ~ SSlogis(x, phi1, phi2, phi3))
+  
+  #C: expect amount of registrations
+  C <- summary(log.ss)$coef[1]
+  #a
+  A <- exp((summary(log.ss)$coef[2]) * (1/summary(log.ss)$coef[3]))
+  #k
+  K <- (1 / summary(log.ss)$coef[3])
+
+  png("n_registrations_in_time_and_predictions.png")  
+  plot(
+    y ~ x, 
+    main = "Expected amount of registrations", 
+    xlab = ind, 
+    ylab = dep, 
+    xlim = c(0, x_max),
+    ylim = c(0, C)
+  )
+  lines(0:x_max, predict(log.ss, data.frame(x=0:x_max)), col="red")
+  dev.off()  
+}
